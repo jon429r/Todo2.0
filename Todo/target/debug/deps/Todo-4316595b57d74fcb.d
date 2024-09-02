fbd73ff768b410137831d75f1af08696a5f95331
jon429r <jonathanday088@gmail.com>
2024-09-01 22:22:23 -0400

    I am batman

---
+++ Todo/target/debug/deps/Todo-4316595b57d74fcb.d
@@ -0,0 +1,5 @@
+/Users/jonathanday/Todo2.0/Todo/target/debug/deps/libTodo-4316595b57d74fcb.rmeta: src/main.rs
+
+/Users/jonathanday/Todo2.0/Todo/target/debug/deps/Todo-4316595b57d74fcb.d: src/main.rs
+
+src/main.rs:
---
+++ Todo/target/debug/deps/Todo-8a51f653d5ef41a2.d
@@ -0,0 +1,5 @@
+/Users/jonathanday/Todo2.0/Todo/target/debug/deps/libTodo-8a51f653d5ef41a2.rmeta: src/main.rs
+
+/Users/jonathanday/Todo2.0/Todo/target/debug/deps/Todo-8a51f653d5ef41a2.d: src/main.rs
+
+src/main.rs:
